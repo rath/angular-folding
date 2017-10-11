975d621c2b2aa8b8314742a6f1e9d1ff4a6441af
Jang-Ho Hwang <rath.io@kakaocorp.com>
2017-10-11 10:38:05 +0900

    Initial commit

---
+++ src/com/xrath/plugin/fold/ComponentFileGroup.java
@@ -0,0 +1,70 @@
+package com.xrath.plugin.fold;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.projectView.PresentationData;
+import com.intellij.ide.projectView.ProjectViewNode;
+import com.intellij.ide.projectView.ViewSettings;
+import com.intellij.ide.projectView.impl.nodes.PsiFileNode;
+import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComponentFileGroup extends ProjectViewNode<PsiFile> {
+    private final String name;
+    private List<AbstractTreeNode> children;
+
+    protected ComponentFileGroup(Project project, ViewSettings viewSettings, PsiFile directory, String name) {
+        super(project, directory, viewSettings);
+        this.name = name;
+        children = new ArrayList<>();
+    }
+
+    @Override
+    public boolean contains(@NotNull VirtualFile file) {
+        for (AbstractTreeNode childNode : children) {
+            ProjectViewNode treeNode = (ProjectViewNode) childNode;
+            if (treeNode.contains(file)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addChild(AbstractTreeNode node, String extension) {
+        if (node instanceof PsiFileNode) {
+            PsiFileNode n  = (PsiFileNode) node;
+            children.add(new NamedFileNode(n.getProject(), n.getValue(), n.getSettings(), "." + extension));
+        }
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractTreeNode> getChildren() {
+        return children;
+    }
+
+    @Override
+    protected void update(PresentationData presentation) {
+        presentation.setPresentableText(name);
+        presentation.setIcon(AllIcons.Nodes.Class);
+    }
+
+    static class NamedFileNode extends PsiFileNode {
+        private final String name;
+
+        public NamedFileNode(Project project, PsiFile psiFile, ViewSettings viewSettings, String name) {
+            super(project, psiFile, viewSettings);
+            this.name = name;
+        }
+        @Override
+        public void update(PresentationData presentationData) {
+            super.update(presentationData);
+            presentationData.setPresentableText(this.name);
+        }
+    }
+}
---
+++ src/com/xrath/plugin/fold/TreeStructureProvider.java
@@ -0,0 +1,58 @@
+package com.xrath.plugin.fold;
+
+import com.intellij.ide.projectView.ViewSettings;
+import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TreeStructureProvider implements com.intellij.ide.projectView.TreeStructureProvider {
+    private final Pattern namePattern =
+            Pattern.compile("(.*)\\.component\\.(css|sass|less|html|ts)", Pattern.CASE_INSENSITIVE);
+
+    @NotNull
+    @Override
+    public Collection<AbstractTreeNode> modify(@NotNull AbstractTreeNode parent, @NotNull Collection<AbstractTreeNode> children, ViewSettings viewSettings) {
+        if (!(parent.getValue() instanceof PsiDirectory))
+            return children;
+
+        List<AbstractTreeNode> ret = new ArrayList<>();
+        Map<String, ComponentFileGroup> map = new HashMap<>();
+        for (AbstractTreeNode child : children) {
+            if (!(child.getValue() instanceof PsiFile)) {
+                ret.add(child);
+                continue;
+            }
+
+            PsiFile psiFile = (PsiFile) child.getValue();
+            String filename = psiFile.getName();
+            Matcher m = namePattern.matcher(filename);
+            if (!m.find()) {
+                ret.add(child);
+                continue;
+            }
+
+            String prefix = m.group(1);
+            String extension = m.group(2);
+            ComponentFileGroup group = map.get(prefix);
+            if (group == null) {
+                group = new ComponentFileGroup(child.getProject(), viewSettings, psiFile, prefix + ".component");
+                map.put(prefix, group);
+                ret.add(group);
+            }
+            group.addChild(child, extension);
+        }
+        return ret;
+    }
+
+    @Nullable
+    @Override
+    public Object getData(Collection<AbstractTreeNode> selected, String dataName) {
+        return null;
+    }
+}
