203cd34416b036d91903e08e224d73baf857063a
rath <rath@xrath.com>
2024-12-15 19:46:57 +0100

    Polish details of Setting UI

--- src/main/java/com/xrath/plugin/fold/settings/PluginSettingsComponent.java
+++ src/main/java/com/xrath/plugin/fold/settings/PluginSettingsComponent.java
@@ -1,16 +1,25 @@
 package com.xrath.plugin.fold.settings;
 
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.components.JBTextArea;
+import com.intellij.util.ui.FormBuilder;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class PluginSettingsComponent {
     private final JPanel mainPanel;
-    private final JTextField namePatternField;
+    private final JTextArea namePatternField;
 
     public PluginSettingsComponent() {
-        mainPanel = new JPanel();
-        namePatternField = new JTextField(40);
-        mainPanel.add(new JLabel("Name Pattern:"));
-        mainPanel.add(namePatternField);
+        namePatternField = new JBTextArea(5, 20);
+        namePatternField.setLineWrap(true);
+        namePatternField.setWrapStyleWord(false);
+
+        JPanel panel = FormBuilder.createFormBuilder().addLabeledComponent(
+                "Regex for grouping files", new JBScrollPane(namePatternField)).getPanel();
+        mainPanel = new JPanel(new BorderLayout());
+        mainPanel.add(panel, BorderLayout.NORTH);
     }
 
     public JPanel getPanel() {
